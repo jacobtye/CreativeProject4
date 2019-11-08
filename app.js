0ef97d9ac86987473e59815526af20e9c1d58ddb
jacobtye <jacob.h.tye@gmail.com>
2019-11-08 06:21:15 +0000

    done

---
+++ app.js
@@ -0,0 +1,101 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const bodyParser = require('body-parser');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+let docs = [];
+let temp = "";
+let changed = false;
+app.post('/saveDocument', (req, res) => {
+    console.log("in post");
+    console.log(req.body);
+  let item = {
+    name: req.body.name,
+    text: req.body.text,
+  };
+  docs.push(item);
+  console.log(docs);
+  res.send(item);
+});
+app.post('/saveTemp', (req, res) => {
+    console.log("in post");
+    console.log(req.body);
+  temp = req.body.text;
+  console.log(temp);
+  this.changed = true;
+  res.send(temp);
+});
+app.get('/getTemp', (req,res) =>{
+      console.log("in get");
+    console.log(req.body);
+  console.log(temp);
+  res.send(temp);
+});
+app.get('/getDocuments', (req, res) => {
+    console.log("in get");
+    console.log(req.body);
+  console.log(docs);
+  res.send(docs);
+});
+app.get('/getChanged', (req, res) => {
+    console.log("in get");
+    console.log(req.body);
+    console.log(this.changed);
+  
+  res.send(this.changed);
+  if(this.changed){
+    this.changed = false;
+  }
+});
+app.put('/deleteFile', (req,res) => {
+  console.log("IN DELETE");
+  console.log(docs.length);
+  res.send(docs.splice(req.body.index, 1));
+  console.log(docs.length);
+  console.log("DONE");
+});
+app.use('/', indexRouter);
+console.log("here");
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ public/javascripts/app.js
@@ -0,0 +1,153 @@
+/*global axios */
+/*global Vue */
+var app = new Vue({
+  el: '#app',
+  data: {
+    docs: [],
+    words : "",
+    file : "",
+    saved : true,
+    currentFile : "",
+  },
+  created: function() {
+    this.updateFiles();
+    console.log(this.docs);
+      
+
+  },
+  watch: {
+    words(value, oldvalue) {
+        this.saved = false;
+    },
+  },
+  methods: {
+    async saveAs(){
+        if (this.file == "" && this.currentFile == ""){
+            this.file = prompt("Please enter file name", "");
+        }
+        if (this.file == "" || this.file == null){
+            return;
+        }
+        let docsMap = this.docs.map(item => {
+            return item.name;
+        });
+        let index = docsMap.indexOf(this.file);
+        if(index != -1){
+            if(!confirm("Are you sure you want to overwirte " + this.file)){
+                return;
+            }
+        }
+        this.currentFile = this.file;
+        await this.save();
+    },
+    async save() {
+        if (this.currentFile == ""){
+            this.saveAs();
+            return;
+        }
+        console.log("in save");
+        let docsMap = this.docs.map(item => {
+            return item.name;
+        });
+        this.file = this.currentFile;
+        let index = docsMap.indexOf(this.file);
+        if(index != -1){
+            await this.deleteFileNoComfirm();
+        }
+        try {
+        const response = await axios.post("/saveDocument", {
+          name: this.currentFile,
+          text: this.words,
+        });
+        alert("Saved to " + this.currentFile);
+        this.file = "";
+        this.saved = true;
+        this.updateFiles();
+      } catch (error) {
+        console.log(error);
+        alert("Error Saving to " + this.currentFile);
+      }
+        
+    },
+    async updateFiles(){
+        try{
+        const response = await axios.get("/getDocuments");
+            this.docs = response.data;
+        }catch(error){
+            console.log(error);
+            alert("Error Getting Files");
+        }
+    },
+    async loadFile(){
+        if (this.file == ""){
+            this.file = prompt("Please enter file name", "");
+        }
+        if (this.file == "" || this.file == null){
+            return;
+        }
+        console.log("DOCS");
+        console.log(this.docs);
+        let docsMap = this.docs.map(item => {
+            return item.name;
+        });
+        let i = docsMap.indexOf(this.file);
+        this.words = this.docs[i].text;
+        this.currentFile = this.file;
+        this.file = "";
+        this.saved = true;
+    },
+    async deleteFile(){
+        console.log("IN DELETE");
+        if(!confirm("Are you sure you want to delete " + this.file)){
+            return;
+        }
+        let docsMap = this.docs.map(item => {
+            return item.name;
+        });
+        let index = docsMap.indexOf(this.file);
+        try {
+            const response = await axios.put("/deleteFile", {
+            index: index,
+        });
+        alert("Deleted " + this.file);
+        this.file = "";
+        this.updateFiles();
+      } catch (error) {
+        console.log(error);
+        alert("Error Deleting " + this.file);
+      }
+    },
+    async deleteFileNoComfirm(){
+        console.log("IN DELETE");
+        let docsMap = this.docs.map(item => {
+            return item.name;
+        });
+        let index = docsMap.indexOf(this.file);
+        try {
+            const response = await axios.put("/deleteFile", {
+            index: index,
+        });
+        this.file = "";
+        this.updateFiles();
+      } catch (error) {
+        console.log(error);
+        alert("Error Deleting " + this.file);
+      }
+    },
+    async newFile(){
+        if (!this.saved){
+            if (confirm("Would you like to save first?")){
+                this.save();
+            }
+        }
+        this.words = "";
+        this.saved = true;
+    },
+    async saveTemp(){
+        console.log("in temp" + this.words);
+        const response = await axios.post("/saveTemp", {
+          text: this.words,
+        });
+    }
+  }
+});
